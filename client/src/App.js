46790b937a3742bf9b60d0443948dcccf3f43524
EvanHendersonRichtByte <adamhenderson3x3@gmail.com>
2021-02-25 23:31:03 +0700

    fix duplicate master branch

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container vh-100 d-flex justify-content-center align-items-center">
+        <h1>Report It!</h1>
+      </div>
+    </div>
+  );
+}
