e67f108fb244446ee0cef9ddfb21eac4289932c9
EvanHendersonRichtByte <adamhenderson3x3@gmail.com>
2021-03-20 03:40:29 +0700

    update response schema

--- server/model/Response.js
+++ server/model/Response.js
@@ -1,7 +1,7 @@
 module.exports = Response = ({ model, Schema }) => {
   const ResponseSchema = new Schema({
     complaint_id: String,
-    employee_id: String,
+    user_id: String,
     response_text: String,
     response_date: { type: Date, default: Date.now() },
   });
